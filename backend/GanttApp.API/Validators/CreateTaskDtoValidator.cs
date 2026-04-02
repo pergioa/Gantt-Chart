6f33f922a76c95eff2f21682b06e24718f5d4a02
Sergio Abreo Alvarez <pergioa@gmail.com>
2026-04-02 13:45:43 -0400

    fix formatting issues

--- backend/GanttApp.API/Validators/CreateTaskDtoValidator.cs
+++ backend/GanttApp.API/Validators/CreateTaskDtoValidator.cs
@@ -7,16 +7,14 @@ public class CreateTaskDtoValidator : AbstractValidator<CreateTaskDto>
 {
     public CreateTaskDtoValidator()
     {
-        RuleFor(t => t.Title)
-                            .NotEmpty()
-                            .MaximumLength(512);
+        RuleFor(t => t.Title).NotEmpty().MaximumLength(512);
 
         RuleFor(t => t.StartDate).NotEmpty();
         RuleFor(t => t.EndDate)
-                              .NotEmpty()
-                              .GreaterThan(t => t.StartDate)
-                              .WithMessage("EndDate must be greater than StartDate");
-        
+            .NotEmpty()
+            .GreaterThan(t => t.StartDate)
+            .WithMessage("EndDate must be greater than StartDate");
+
         RuleFor(t => t.Progress).InclusiveBetween(0, 100);
     }
 }
